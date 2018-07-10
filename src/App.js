92c561542fafea4ac5a3a642f45d97df5be93527
zdp1243 <jjpower.writes@gmail.com>
2018-07-10 14:53:08 -0700

    added chart instances and put in dummy data in instances

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">iSubscribe Charts</h1>
+        
         </header>
+       
         <p className="App-intro">
-        The pie chart shows percentage of total subscription useage.  The line chart shows subscriptions by category.
+        The pie chart shows percentage of total subscription useage.  The bar chart shows subscriptions by category.
         </p>
+
+        <div id="chartContainer">Bar chart will load here!</div>
+
+        <div id="piechartContainer">Pie chart will load here!</div>
+
       </div>
     );
   }
