badc4cd83058d5235e5f04693bae3ded2c3d4aa0
zdp1243 <jjpower.writes@gmail.com>
2018-07-10 11:14:07 -0700

    added FusionChartsXT Library and yarn

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
+          <h1 className="App-title">iSubscribe Charts</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        The pie chart shows percentage of total subscription useage.  The line chart shows subscriptions by category.
         </p>
       </div>
     );
